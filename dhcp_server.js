4cc5b37dcfbe8806cc90390189fb53eedf8a3ffa
Arthur Flachs <arthur.flachs@gmail.com>
2015-03-15 23:09:42 +0100

    Reset server

--- dhcp_server.js
+++ dhcp_server.js
@@ -1,18 +1,6 @@
 var dgram = require('dgram');
 
 var server = dgram.createSocket('udp4');
-server.on('error', function(err) {
-	console.log("server error:\n" + err.stack);
-	server.close();
-});
-server.on('message', function(message, rinfo) {
-    console.log('server got message : ' + message + ' from ' + rinfo.address + ':' + rinfo.port);
-});
-
-server.on('listening', function() {
-	var address = server.address();
-	console.log('server listening on ' + address.address + ':' + address.port);
-});
 
 server.bind(67);
 
