8752105ccffd64cae7532ea98b2a4d073ba0c975
Arthur Flachs <arthur.flachs@gmail.com>
2015-03-15 23:12:02 +0100

    Logs when server available

--- dhcp_server.js
+++ dhcp_server.js
@@ -2,5 +2,12 @@ var dgram = require('dgram');
 
 var server = dgram.createSocket('udp4');
 
+// Logs when server available
+server.on('listening', function() {
+    var address = server.address();
+    console.log('server listening on '
+        + address.address + ':' + address.port);
+});
+
 server.bind(67);
 
